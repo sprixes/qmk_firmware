49a4fd6e48f810c1a3342003011842fd066a86fa
arjay.m3 <arjay.paulino@cybilltek.com>
2026-07-10 01:13:39 +0800

    disable usevia

--- keyboards/preonic/keymaps/sprixes/rules.mk
+++ keyboards/preonic/keymaps/sprixes/rules.mk
@@ -8,10 +8,4 @@ MOUSEKEY_ENABLE = yes
 VIA_ENABLE = no
 
 USER_NAME := sprixes
-
-# Scanning frequency (measured with DEBUG_MATRIX_SCAN_RATE, minimum while
- # typing):
- # sym_eager_pr: 990Hz
- # asym_eager_defer_pk: 955Hz
 DEBOUNCE_TYPE = asym_eager_defer_pk
-
